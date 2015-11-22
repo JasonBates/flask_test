5fb87a7eda3047b031d8ee307eaafa82289a5211
= <=>
2015-11-22 19:19:19 +0000

    test more routes and url_for

--- hello.py
+++ hello.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return "name " + username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return "post " + str(post_id)
+
 @app.route('/')
-def index():
-    return 'Index Page'
+def show_url_for():
+    return url_for('show_user_profile', username = 'Jason')
 
 @app.route('/hello')
 def hello():
