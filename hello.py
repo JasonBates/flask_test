1ea6a073d127bc66abec56c15be20bd7c319a923
= <=>
2015-11-22 18:58:50 +0000

    different routes

--- hello.py
+++ hello.py
@@ -3,9 +3,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def helloworld():
-    return 'Hello World 2!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World!'
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host="192.168.1.83",port=5000)
 
